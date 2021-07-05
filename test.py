5c13e139d81748a7a2e2e122f6a60f4487f8c107
Kabiir <kabiirk@gmail.com>
2021-07-05 20:02:01 +0530

    Added Pixel Grid + Zoom in test.py

--- test.py
+++ test.py
@@ -1,88 +1,119 @@
-import sys
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
-from PyQt5.QtCore import Qt
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-COLORS = [
-# 17 undertones https://lospec.com/palette-list/17undertones
-'#000000', '#141923', '#414168', '#3a7fa7', '#35e3e3', '#8fd970', '#5ebb49',
-'#458352', '#dcd37b', '#fffee5', '#ffd035', '#cc9245', '#a15c3e', '#a42f3b',
-'#f45b7a', '#c24998', '#81588d', '#bcb0c2', '#ffffff',
-]
+class PhotoViewer(QtWidgets.QGraphicsView):
+    photoClicked = QtCore.pyqtSignal(QtCore.QPoint)
+
+    def __init__(self, parent):
+        super(PhotoViewer, self).__init__(parent)
+        self._zoom = 0
+        self._empty = True
+        self._scene = QtWidgets.QGraphicsScene(self)
+        self._photo = QtWidgets.QGraphicsPixmapItem()
+        self._scene.addItem(self._photo)
+        self.setScene(self._scene)
+        self.setTransformationAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+        self.setResizeAnchor(QtWidgets.QGraphicsView.AnchorUnderMouse)
+        self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.setBackgroundBrush(QtGui.QBrush(QtGui.QColor(30, 30, 30)))
+        self.setFrameShape(QtWidgets.QFrame.NoFrame)
+
+    def hasPhoto(self):
+        return not self._empty
+
+    def fitInView(self, scale=True):
+        rect = QtCore.QRectF(self._photo.pixmap().rect())
+        if not rect.isNull():
+            self.setSceneRect(rect)
+            if self.hasPhoto():
+                unity = self.transform().mapRect(QtCore.QRectF(0, 0, 1, 1))
+                self.scale(1 / unity.width(), 1 / unity.height())
+                viewrect = self.viewport().rect()
+                scenerect = self.transform().mapRect(rect)
+                factor = min(viewrect.width() / scenerect.width(),
+                             viewrect.height() / scenerect.height())
+                self.scale(factor, factor)
+            self._zoom = 0
+
+    def setPhoto(self, pixmap=None):
+        self._zoom = 0
+        if pixmap and not pixmap.isNull():
+            self._empty = False
+            self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+            self._photo.setPixmap(pixmap)
+        else:
+            self._empty = True
+            self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
+            self._photo.setPixmap(QtGui.QPixmap())
+        self.fitInView()
+
+    def wheelEvent(self, event):
+        if self.hasPhoto():
+            if event.angleDelta().y() > 0:
+                factor = 1.25
+                self._zoom += 1
+            else:
+                factor = 0.8
+                self._zoom -= 1
+            if self._zoom > 0:
+                self.scale(factor, factor)
+            elif self._zoom == 0:
+                self.fitInView()
+            else:
+                self._zoom = 0
+
+    def toggleDragMode(self):
+        if self.dragMode() == QtWidgets.QGraphicsView.ScrollHandDrag:
+            self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
+        elif not self._photo.pixmap().isNull():
+            self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+
+    def mousePressEvent(self, event):
+        if self._photo.isUnderMouse():
+            self.photoClicked.emit(self.mapToScene(event.pos()).toPoint())
+        super(PhotoViewer, self).mousePressEvent(event)
 
 
-class QPaletteButton(QtWidgets.QPushButton):
-
-    def __init__(self, color):
-        super().__init__()
-        self.setFixedSize(QtCore.QSize(24,24))
-        self.color = color
-        self.setStyleSheet("background-color: %s;" % color)
-
-
-class Canvas(QtWidgets.QLabel):
-    
+class Window(QtWidgets.QWidget):
     def __init__(self):
-        super().__init__()
-        pixmap = QtGui.QPixmap(600, 300)
-        pixmap.fill(QtGui.QColor("white"))
-        self.setPixmap(pixmap)
+        super(Window, self).__init__()
+        self.viewer = PhotoViewer(self)
+        # 'Load image' button
+        self.btnLoad = QtWidgets.QToolButton(self)
+        self.btnLoad.setText('Load image')
+        self.btnLoad.clicked.connect(self.loadImage)
+        # Button to change from drag/pan to getting pixel info
+        self.btnPixInfo = QtWidgets.QToolButton(self)
+        self.btnPixInfo.setText('Enter pixel info mode')
+        self.btnPixInfo.clicked.connect(self.pixInfo)
+        self.editPixInfo = QtWidgets.QLineEdit(self)
+        self.editPixInfo.setReadOnly(True)
+        self.viewer.photoClicked.connect(self.photoClicked)
+        # Arrange layout
+        VBlayout = QtWidgets.QVBoxLayout(self)
+        VBlayout.addWidget(self.viewer)
+        HBlayout = QtWidgets.QHBoxLayout()
+        HBlayout.setAlignment(QtCore.Qt.AlignLeft)
+        HBlayout.addWidget(self.btnLoad)
+        HBlayout.addWidget(self.btnPixInfo)
+        HBlayout.addWidget(self.editPixInfo)
+        VBlayout.addLayout(HBlayout)
 
-        self.last_x, self.last_y = None, None
-        self.pen_color = QtGui.QColor('#000000')
+    def loadImage(self):
+        self.viewer.setPhoto(QtGui.QPixmap(r'C:\\Users\\kabii\Desktop\\a.png'))
 
-    def set_pen_color(self, c):
-        self.pen_color = QtGui.QColor(c)
+    def pixInfo(self):
+        self.viewer.toggleDragMode()
 
-    def mouseMoveEvent(self, e):
-        if self.last_x is None: # First event.
-            self.last_x = e.x()
-            self.last_y = e.y()
-            return # Ignore the first time.
-
-        painter = QtGui.QPainter(self.pixmap())
-        p = painter.pen()
-        p.setWidth(4)
-        p.setColor(self.pen_color)
-        painter.setPen(p)
-        painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
-        painter.end()
-        self.update()
-
-        # Update the origin for next time.
-        self.last_x = e.x()
-        self.last_y = e.y()
-
-    def mouseReleaseEvent(self, e):
-        self.last_x = None
-        self.last_y = None
-
-
-class MainWindow(QtWidgets.QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-
-        self.canvas = Canvas()
-
-        w = QtWidgets.QWidget()
-        l = QtWidgets.QVBoxLayout()
-        w.setLayout(l)
-        l.addWidget(self.canvas)
-
-        palette = QtWidgets.QHBoxLayout()
-        self.add_palette_buttons(palette)
-        l.addLayout(palette)
-
-        self.setCentralWidget(w)
-
-    def add_palette_buttons(self, layout):
-        for c in COLORS:
-            b = QPaletteButton(c)
-            b.pressed.connect(lambda c=c: self.canvas.set_pen_color(c))
-            layout.addWidget(b)
+    def photoClicked(self, pos):
+        if self.viewer.dragMode()  == QtWidgets.QGraphicsView.NoDrag:
+            self.editPixInfo.setText('%d, %d' % (pos.x(), pos.y()))
 
 
-app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
-window.show()
-app.exec_()
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = Window()
+    window.setGeometry(500, 300, 800, 600)
+    window.show()
+    sys.exit(app.exec_())
--- ui/ui.py
+++ ui/ui.py
@@ -6,56 +6,10 @@
 import sys
 from PyQt5 import QtWidgets
 
-from PyQt5.QtGui import *
+from PyQt5.QtGui import QBrush, QPainter, QPen
 from PyQt5.QtCore import *
-from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView, QMenuBar, QMenu, QAction, QLabel, QWidget
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsItem, QMenuBar, QMenu, QAction, QLabel, QWidget
 
-NUM_BLOCKS_X = 10
-NUM_BLOCKS_Y = 10
-WIDTH = 20
-HEIGHT = 20
-
-
-class QS(QGraphicsScene):
-    
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.lines = []
-
-        self.draw_grid()
-        self.set_opacity(0.3)
-        #self.set_visible(False)
-        #self.delete_grid()
-
-    def draw_grid(self):
-        width = NUM_BLOCKS_X * WIDTH
-        height = NUM_BLOCKS_Y * HEIGHT
-        self.setSceneRect(0, 0, width, height)
-        self.setItemIndexMethod(QGraphicsScene.NoIndex)
-
-        pen = QPen(QColor(255,0,100), 1, Qt.SolidLine)
-
-        for x in range(0,NUM_BLOCKS_X+1):
-            xc = x * WIDTH
-            self.lines.append(self.addLine(xc,0,xc,height,pen))
-
-        for y in range(0,NUM_BLOCKS_Y+1):
-            yc = y * HEIGHT
-            self.lines.append(self.addLine(0,yc,width,yc,pen))
-
-    def set_visible(self,visible=True):
-        for line in self.lines:
-            line.setVisible(visible)
-
-    def delete_grid(self):
-        for line in self.lines:
-            self.removeItem(line)
-        del self.lines[:]
-
-    def set_opacity(self,opacity):
-        for line in self.lines:
-            line.setOpacity(opacity)
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -63,127 +17,26 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("Pyxel")
         self.setGeometry(200,200,400,400)
 
-        # 1. Menubar
-        #   1.A. Creating Menubar & Menu Items
-        self.menubar = QMenuBar(self)
-        #   File
-        filemenu = QMenu('&File', self.menubar)
-        self.newAction = QAction("New", self)
-        self.openAction = QAction("Open", self)
-        self.saveAction = QAction("Save", self)
-        self.exitAction = QAction("Exit", self)
-        filemenu.addAction(self.newAction)
-        filemenu.addAction(self.openAction)
-        filemenu.addAction(self.saveAction)
-        filemenu.addAction(self.exitAction)
+        self._zoom = 0
 
-        #   Edit
-        editmenu = QMenu('&Edit', self.menubar)
-        self.copyAction = QAction("Copy", self)
-        self.pasteAction = QAction("Paste", self)
-        self.cutAction = QAction("Cut", self)
-        editmenu.addAction(self.copyAction)
-        editmenu.addAction(self.pasteAction)
-        editmenu.addAction(self.cutAction)
-        #   Help
-        helpmenu = QMenu('&Help', self.menubar)
-        self.helpAction = QAction("Help", self)
-        self.aboutAction = QAction("About", self)
-        helpmenu.addAction(self.helpAction)
-        helpmenu.addAction(self.aboutAction)
+        self.gs = QGraphicsScene()
 
-        #    1.B. Adding Menu Items to Menubar
-        self.menubar.addMenu(filemenu)
-        self.menubar.addMenu(editmenu)
-        self.menubar.addMenu(helpmenu)
+        self.brush1 = QBrush(Qt.white)
+        self.brush2 = QBrush(Qt.black)
 
-        # Setting menubar as Menubar for Window
-        self.setMenuBar(self.menubar)
+        self.pen = QPen(Qt.red)
 
-        
-        # Binding Actions in Menubar + Toolbars to Functions
-        self.newAction.triggered.connect(self.newFile)
-        self.openAction.triggered.connect(self.openFile)
-        self.saveAction.triggered.connect(self.saveFile)
-        self.exitAction.triggered.connect(self.close)
-        # Connect Edit actions
-        self.copyAction.triggered.connect(self.copyContent)
-        self.pasteAction.triggered.connect(self.pasteContent)
-        self.cutAction.triggered.connect(self.cutContent)
-        # Connect Help actions
-        self.helpAction.triggered.connect(self.helpContent)
-        self.aboutAction.triggered.connect(self.about)
+        gv = QGraphicsView(self.gs, self)
 
-        # Assigning Keyboard Shortcuts to Actions
-        self.newAction.setShortcut("Ctrl+N")
-        self.openAction.setShortcut("Ctrl+O")
-        self.saveAction.setShortcut("Ctrl+S")
-        # Edit actions
-        # Using standard keys
-        self.copyAction.setShortcut(QKeySequence.Copy)
-        self.pasteAction.setShortcut(QKeySequence.Paste)
-        self.cutAction.setShortcut(QKeySequence.Cut)
+        for i in range(32):
+            for j in range(32):
+                rect = self.gs.addRect(i*20, j*20, 20, 20, self.pen, self.brush1)
+                rect.setAcceptHoverEvents(True)
+        # rect1 = self.gs.addRect(10,10,20,20, self.pen, self.brush1)
+        # rect2 = self.gs.addRect(30,10,20,20, self.pen, self.brush1)
+        # rect3 = self.gs.addRect(50,10,20,20, self.pen, self.brush1)
+        # rect1.setFlag(QGraphicsItem.ItemIsMovable, QGraphicsItem.ItemIsSelectable)
 
-
-        # self.label = QLabel()
-        # self.label.setStyleSheet("background-color: lightgreen")
-        # self.label.setSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
-        # self.label.setAlignment(Qt.AlignCenter)
-        # self.setMinimumSize(10,10)
-        # w = self.label.width()
-        # h = self.label.height()
-        # self.canvas = QPixmap(w, h)
-        # self.canvas.fill(QColor("white")) # Ref.: https://stackoverflow.com/questions/63269098/qpixmap-qpainter-showing-black-window-background
-        # self.label.setPixmap(self.canvas.scaled(w,h ))
-        # self.setCentralWidget(self.label)
-        # self.label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-        
-        # #self.draw_something()
-        # self.last_x, self.last_y = None, None
-        # print(self.label.size(), w, h)
-        gv = QGraphicsView()
-        gs = QS()
-        gv.setScene(gs)
         self.setCentralWidget(gv)
 
-
-
-    def newFile(self):
-        # Logic for creating a new file goes here...
-        print("Pressed !")
-        # self.centralWidget.setText("<b>File > New</b> clicked")
-
-    def openFile(self):
-        # Logic for opening an existing file goes here...
-        print("Pressed !")
-        # self.centralWidget.setText("<b>File > Open...</b> clicked")
-
-    def saveFile(self):
-        # Logic for saving a file goes here...
-        print("Pressed !")
-        # self.centralWidget.setText("<b>File > Save</b> clicked")
-
-    def copyContent(self):
-        # Logic for copying content goes here...
-        print("Pressed !")
-        # self.centralWidget.setText("<b>Edit > Copy</b> clicked")
-
-    def pasteContent(self):
-        # Logic for pasting content goes here...
-        print("Pressed !")
-        # self.centralWidget.setText("<b>Edit > Paste</b> clicked")
-
-    def cutContent(self):
-        # Logic for cutting content goes here...
-        print("Pressed !")
-        # self.centralWidget.setText("<b>Edit > Cut</b> clicked")
-
-    def helpContent(self):
-        # Logic for launching help goes here...
-        print("Pressed !")
-        # self.centralWidget.setText("<b>Help > Help Content...</b> clicked")
-
-    def about(self):
-        # Logic for showing an about dialog content goes here...
-        print("Pressed !")
-        # self.centralWidget.setText("<b>Help > About...</b> clicked")
+       
