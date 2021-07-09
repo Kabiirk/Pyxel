405ea072647569002ee22db015c34f87b5729cfa
Kabiir <kabiirk@gmail.com>
2021-07-10 00:02:15 +0530

    VERY Rudimentary pixel art editor implemented

--- ui/ui.py
+++ ui/ui.py
@@ -17,15 +17,19 @@ from PyQt5.QtWidgets import QGraphicsRectItem, QGraphicsScene, QGraphicsView, QG
 class Pixel(QGraphicsRectItem):
     def __init__(self, x,y,h,w, parent=None):
         super().__init__(x,y,h,w, parent=None)
+        self.default_color = QBrush(Qt.white)
         self.setAcceptHoverEvents(True)
-        self.setBrush(QBrush(Qt.white))
+        self.setBrush(self.default_color)
 
     def hoverEnterEvent(self, event):
         self.setBrush(QBrush(Qt.black))
-        print('hello')
     
     def hoverLeaveEvent(self, event):
-        self.setBrush(QBrush(Qt.white))
+        self.setBrush(self.default_color)
+
+    def mousePressEvent(self, event):
+        self.setBrush(QBrush(Qt.red))
+        self.default_color = QBrush(Qt.red)
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -36,6 +40,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self._zoom = 0
 
         self.gs = QGraphicsScene()
+        self.gs.setBackgroundBrush(QBrush(Qt.blue))
 
         self.brush1 = QBrush(Qt.white)
         self.brush2 = QBrush(Qt.black)
@@ -43,10 +48,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.pen = QPen(Qt.red)
 
         self.gv = QGraphicsView(self.gs)
-        a = Pixel(10,10,120,120)
-        #a.setBrush(self.brush1)
-        # a.setAcceptHoverEvents(True)
-        self.gs.addItem(a)
+        for i in range(20):
+            for j in range(20):
+                self.gs.addItem(Pixel(i*20, j*20,20,20))
 
         self.setCentralWidget(self.gv)
 
